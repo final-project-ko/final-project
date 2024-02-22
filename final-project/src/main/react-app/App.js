4f748945b9991a50fd63fedb401887ca302e994e
baekjonghwan <baekfive@gmail.com>
2024-02-22 13:50:21 +0900

    no message

--- final-project/src/main/react-app/App.js
+++ final-project/src/main/react-app/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Weather from "./weather/Weather";
+import Weather from "./pages/Weather";
 
 export default function App() {
 
