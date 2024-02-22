3fb21c5cc52e4a2af0f09cd38ce547a587978b02
10041107 <aksmfdyfl111@gmail.com>
2024-02-22 18:33:28 +0900

    test

--- final-project/src/main/react-app/App.js
+++ final-project/src/main/react-app/App.js
@@ -12,32 +12,26 @@ npm install react-native-safe-area-context
 */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
 import Weather from "./apis/WeatherAPI";
-=======
-import { Button } from 'react-native-elements';
+// import { Button } from 'react-native-elements';
 
-const today = new Date(); 
-const month = today.getMonth() + 1; 
-const date = today.getDate();
-const ToDayDate = month + '월' + date + '일';
->>>>>>> 3fa17b41500bdc5292d95ebe6c79474075a714fe
+// const today = new Date(); 
+// const month = today.getMonth() + 1; 
+// const date = today.getDate();
+// const ToDayDate = month + '월' + date + '일';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-<<<<<<< HEAD
-=======
-      <View style={styles.buttonContainer}>
-        <Button title="ToDayDate" type={ ToDayDate } />
-      </View>
-      <Text>
-        ㅅㄷ
-      </Text>
->>>>>>> 3fa17b41500bdc5292d95ebe6c79474075a714fe
-      <StatusBar style="auto" />
-      <Weather style={styles.weather}/>
-    </View>
+    // <View style={styles.container}
+      // <View style={styles.buttonContainer}>
+      //   <Button title="ToDayDate" type={ ToDayDate } />
+      // </View>
+      // <Text>
+      //   ㅅㄷ
+      // </Text>
+    //   <StatusBar style="auto" />
+    //   <Weather style={styles.weather}/>
+    // </View>
   );
 }
 
@@ -48,15 +42,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-<<<<<<< HEAD
-  weather: {
-    border: '1px solid black',
-    width: '80%',
-    height: '25%',
-    position: "absolute",
-    top: '10%'
-  }
-=======
+  // weather: {
+  //   border: '1px solid black',
+  //   width: '80%',
+  //   height: '25%',
+  //   position: "absolute",
+  //   top: '10%'
+  // }
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
