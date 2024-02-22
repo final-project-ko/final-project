0a284ab91dcb7b37289d634ef243b0aa5b078e3c
baekjonghwan <baekfive@gmail.com>
2024-02-22 18:25:28 +0900

    비디오 배경 날씨

--- final-project/src/main/react-app/App.js
+++ final-project/src/main/react-app/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Weather from "./pages/Weather";
+import Weather from "./apis/WeatherAPI";
 
 export default function App() {
 
@@ -9,10 +9,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>
-
-      </Text>
       <StatusBar style="auto" />
+      <Weather style={styles.weather}/>
     </View>
   );
 }
@@ -24,6 +22,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  weather: {
+    border: '1px solid black',
+    width: '80%',
+    height: '25%',
+    position: "absolute",
+    top: '10%'
+  }
 });
 
 
