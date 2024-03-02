6afbc55be4645673b8b97025d50b4b06afd97998
jmsdew <jmsdew@naver.com>
2024-03-02 14:35:35 +0900

    no message

--- final-project/src/main/java/com/ohgiraffers/finalproject/users/userInfo/controller/UserController.java
+++ final-project/src/main/java/com/ohgiraffers/finalproject/users/userInfo/controller/UserController.java
@@ -2,6 +2,9 @@ package com.ohgiraffers.finalproject.users.userInfo.controller;
 
 import com.ohgiraffers.finalproject.login.kakao.entity.UserEntity;
 import com.ohgiraffers.finalproject.users.userInfo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +22,11 @@ public class UserController {
     private UserService userService;
 
 
+    @Operation(summary = "어드민 유저 정보 조회 메소드", description = "전체 유저 정보 조회 메소드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "불러 오기 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 정보")
+    })
     @GetMapping("/admin")
     public List<UserEntity> adminUserInfo(){
         List<UserEntity> userInfo = userService.adminUserInfo();
