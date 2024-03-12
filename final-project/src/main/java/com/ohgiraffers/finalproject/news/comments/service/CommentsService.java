17cf14bf4c7d9f937489fa142f82b8b9d85fde0a
kiyu <giu0428@gmail.com>
2024-03-12 12:14:40 +0900

    댓글 로직 수정

--- final-project/src/main/java/com/ohgiraffers/finalproject/news/comments/service/CommentsService.java
+++ final-project/src/main/java/com/ohgiraffers/finalproject/news/comments/service/CommentsService.java
@@ -21,9 +21,12 @@ public class CommentsService {
 
         Comments registComment = new Comments();
         registComment.setNewsCode(Integer.parseInt(comment.getNewsCode()));
+        registComment.setUserId(comment.getUserId());
         registComment.setEmail(comment.getEmail());
         registComment.setContent(comment.getContent());
         registComment.setDate(LocalDate.now());
+        registComment.setStatus("Y");
+        registComment.setNotify(0);
 
         Comments resultRegistComment = commentsRepository.save(registComment);
 
@@ -37,9 +40,21 @@ public class CommentsService {
                     CommentsDTO commentsDTO = new CommentsDTO();
                     commentsDTO.setCommentCode(comments.getCommentCode());
                     commentsDTO.setNewsCode(comments.getNewsCode());
+                    commentsDTO.setUserId(comments.getUserId());
                     commentsDTO.setEmail(comments.getEmail());
-                    commentsDTO.setContent(comments.getContent());
+
+                    // 조건에 따라 content 설정
+                    if ("Y".equals(comments.getStatus())) {
+                        commentsDTO.setContent(comments.getContent());
+                    } else if ("N".equals(comments.getStatus())) {
+                        commentsDTO.setContent("작성자에 의해 삭제된 댓글입니다.");
+                    } else if ("B".equals(comments.getStatus())) {
+                        commentsDTO.setContent("다수의 신고에 의해 블라인드 처리된 댓글입니다.");
+                    }
+
                     commentsDTO.setDate(comments.getDate());
+                    commentsDTO.setStatus(comments.getStatus());
+                    commentsDTO.setNotify(comments.getNotify());
 
                     return commentsDTO;
                 })
