a6f898bb6b09bbb285e622a4cb9085cebe71f40d
kucu-try <baekfive@gmail.com>
2024-03-13 09:26:19 +0900

    북마크 조회

--- final-project/src/main/java/com/ohgiraffers/finalproject/users/bookmark/repository/BookmarkRepository.java
+++ final-project/src/main/java/com/ohgiraffers/finalproject/users/bookmark/repository/BookmarkRepository.java
@@ -5,8 +5,13 @@ import com.ohgiraffers.finalproject.users.bookmark.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
 
+    List<Bookmark> findByUserId(String userId);
 
+    List<Bookmark> findByNewsCode(Integer newsCode);
 }
