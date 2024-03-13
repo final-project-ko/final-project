fe5bd034227d6d3980079769d1085f5098f8f7ce
jmsdew <jmsdew@naver.com>
2024-03-13 12:15:27 +0900

    젠킨스용 push

--- final-project/src/test/java/com/ohgiraffers/finalproject/FinalProjectApplicationTests.java
+++ final-project/src/test/java/com/ohgiraffers/finalproject/FinalProjectApplicationTests.java
@@ -1,13 +1,25 @@
 package com.ohgiraffers.finalproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FinalProjectApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		// 애플리케이션 컨텍스트가 올바르게 로드되었는지 확인
+		assertThat(context).isNotNull();
+
+		// 예를 들어, 특정 빈이 존재하는지 확인
+		// assertThat(context.getBean(ExampleBean.class)).isNotNull();
 	}
 
 }
