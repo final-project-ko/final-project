d7dcfcfa63744a08cfab17d23025f487a535c2d7
jmsdew <jmsdew@naver.com>
2024-03-30 20:20:29 +0900

    테스트코드

--- final-project/src/test/java/com/ohgiraffers/finalproject/notice/service/NoticeServiceTest.java
+++ final-project/src/test/java/com/ohgiraffers/finalproject/notice/service/NoticeServiceTest.java
@@ -1,7 +1,57 @@
 package com.ohgiraffers.finalproject.notice.service;
 
+import com.ohgiraffers.finalproject.notice.repository.NoticeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class NoticeServiceTest {
+
+    @Mock
+    private NoticeRepository noticeRepository;
+
+    @InjectMocks
+    private NoticeService noticeService;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testInsertNotice(){
+
+        HashMap<String,String> notice = new HashMap<>();
+
+    }
+
+    @Test
+    public void testFindInputNotice(){
+
+    }
+
+    @Test
+    public void testFindAllNotice(){
+
+    }
+
+    @Test
+    public void testModifyNotice(){
+
+    }
+
+    @Test
+    public void testDeleteNotice(){
+        
+    }
 
 }
